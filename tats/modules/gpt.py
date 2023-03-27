e4c8de24df654d763d7e1761a234b9004f6f05d7
zhengdali3 <zhengdali3@gmail.com>
2023-03-27 23:17:02 +0800

    original version

--- tats/modules/gpt.py
+++ tats/modules/gpt.py
@@ -178,85 +178,20 @@ class GPT1Config(GPTConfig):
     n_head = 12
     n_embd = 768
 
-# class CausalSelfAttention(nn.Module):
-#     """
-#     A vanilla multi-head masked self-attention layer with a projection at the end.
-#     It is possible to use torch.nn.MultiheadAttention here but I am including an
-#     explicit implementation here to show that there is nothing too scary here.
-#     """
-
-#     def __init__(self, config):
-#         super().__init__()
-#         assert config.n_embd % config.n_head == 0
-#         # key, query, value projections for all heads
-#         self.key = nn.Linear(config.n_embd, config.n_embd)
-#         self.query = nn.Linear(config.n_embd, config.n_embd)
-#         self.value = nn.Linear(config.n_embd, config.n_embd)
-#         # regularization
-#         self.attn_drop = nn.Dropout(config.attn_pdrop)
-#         self.resid_drop = nn.Dropout(config.resid_pdrop)
-#         # output projection
-#         self.proj = nn.Linear(config.n_embd, config.n_embd)
-#         # causal mask to ensure that attention is only applied to the left in the input sequence
-#         mask = torch.tril(torch.ones(config.block_size,
-#                                      config.block_size))
-#         if hasattr(config, "n_unmasked") and config.n_unmasked > 0:
-#             # mask[:, :config.n_unmasked] = 1
-#             # mask[:, -config.n_unmasked:] = 1
-#             # mask[-config.n_unmasked:, config.n_unmasked:-config.n_unmasked] = 0
-#             mask[:, :config.n_unmasked+1] = 1
-#             mask[:, -config.n_unmasked+1:] = 1
-#             mask[-config.n_unmasked+1:, config.n_unmasked+1:-config.n_unmasked+1] = 0
-#         self.register_buffer("mask", mask.view(1, 1, config.block_size, config.block_size))
-#         self.n_head = config.n_head
-
-#     def forward(self, x, layer_past=None):
-#         B, T, C = x.size()
-
-#         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-#         k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-#         q = self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-#         v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-#         present = torch.stack((k, v))
-#         if layer_past is not None:
-#             past_key, past_value = layer_past
-#             k = torch.cat((past_key, k), dim=-2)
-#             v = torch.cat((past_value, v), dim=-2)
-
-#         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-#         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-#         if layer_past is None:
-#             att = att.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))
-
-#         att = F.softmax(att, dim=-1)
-#         att = self.attn_drop(att)
-#         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-#         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-
-#         # output projection
-#         y = self.resid_drop(self.proj(y))
-#         return y, present   # TODO: check that this does not break anything
-    
-    
 class CausalSelfAttention(nn.Module):
     """
     A vanilla multi-head masked self-attention layer with a projection at the end.
     It is possible to use torch.nn.MultiheadAttention here but I am including an
     explicit implementation here to show that there is nothing too scary here.
     """
-    
-#     focus_cuda0 = getGaussian(T=4, H=16, W=16, beta=[100,100], d=torch.device('cuda', 0))
-#     focus_cuda1 = torch.tensor(focus_cuda0, device=torch.device('cuda', 1))
 
     def __init__(self, config):
         super().__init__()
+        assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads
         self.key = nn.Linear(config.n_embd, config.n_embd)
         self.query = nn.Linear(config.n_embd, config.n_embd)
         self.value = nn.Linear(config.n_embd, config.n_embd)
-        self.focus = None
-        
         # regularization
         self.attn_drop = nn.Dropout(config.attn_pdrop)
         self.resid_drop = nn.Dropout(config.resid_pdrop)
@@ -282,47 +217,86 @@ class CausalSelfAttention(nn.Module):
         k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        
-        # device = v.get_device()
-        
-        # self.focus = CausalSelfAttention.focus_cuda0
-        
-#         if device == 0:
-#             self.focus = CausalSelfAttention.focus_cuda0
-#         else:
-#             self.focus = CausalSelfAttention.focus_cuda1
-#         f_device = CausalSelfAttention.fsocus.get_device()
-        
-        # if device != f_device:
-#             CausalSelfAttention.focus = CausalSelfAttention.focus.to(f"cuda:{device}")
-        
-        # print(device)
 
         present = torch.stack((k, v))
-#         if layer_past is not None:
-#             past_key, past_value = layer_past
-#             k = torch.cat((past_key, k), dim=-2)
-#             v = torch.cat((past_value, v), dim=-2)
-#         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        if layer_past is not None:
+            past_key, past_value = layer_past
+            k = torch.cat((past_key, k), dim=-2)
+            v = torch.cat((past_value, v), dim=-2)
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        
         if layer_past is None:
             att = att.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))
 
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
-        
-        att = focus.focusAttention.apply(att, v)
-        
-        # att = FocusedAttention(att, v, self.focus)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
-        # y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = att.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
- 
         # output projection
-        y = y.to(torch.float32)
         y = self.resid_drop(self.proj(y))
         return y, present   # TODO: check that this does not break anything
+    
+    
+# class CausalSelfAttention(nn.Module):
+#     """
+#     A vanilla multi-head masked self-attention layer with a projection at the end.
+#     It is possible to use torch.nn.MultiheadAttention here but I am including an
+#     explicit implementation here to show that there is nothing too scary here.
+#     """
+
+#     def __init__(self, config):
+#         super().__init__()
+#         # key, query, value projections for all heads
+#         self.key = nn.Linear(config.n_embd, config.n_embd)
+#         self.query = nn.Linear(config.n_embd, config.n_embd)
+#         self.value = nn.Linear(config.n_embd, config.n_embd)
+#         self.focus = None
+        
+#         # regularization
+#         self.attn_drop = nn.Dropout(config.attn_pdrop)
+#         self.resid_drop = nn.Dropout(config.resid_pdrop)
+#         # output projection
+#         self.proj = nn.Linear(config.n_embd, config.n_embd)
+#         # causal mask to ensure that attention is only applied to the left in the input sequence
+#         mask = torch.tril(torch.ones(config.block_size,
+#                                      config.block_size))
+#         if hasattr(config, "n_unmasked") and config.n_unmasked > 0:
+#             # mask[:, :config.n_unmasked] = 1
+#             # mask[:, -config.n_unmasked:] = 1
+#             # mask[-config.n_unmasked:, config.n_unmasked:-config.n_unmasked] = 0
+#             mask[:, :config.n_unmasked+1] = 1
+#             mask[:, -config.n_unmasked+1:] = 1
+#             mask[-config.n_unmasked+1:, config.n_unmasked+1:-config.n_unmasked+1] = 0
+#         self.register_buffer("mask", mask.view(1, 1, config.block_size, config.block_size))
+#         self.n_head = config.n_head
+
+#     def forward(self, x, layer_past=None):
+#         B, T, C = x.size()
+
+#         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+#         k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+#         q = self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+#         v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        
+#         present = torch.stack((k, v))
+#         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        
+#         if layer_past is None:
+#             att = att.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))
+
+#         att = F.softmax(att, dim=-1)
+#         att = self.attn_drop(att)
+        
+#         att = focus.focusAttention.apply(att, v)
+
+#         y = att.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+ 
+#         # output projection
+#         y = y.to(torch.float32)
+#         y = self.resid_drop(self.proj(y))
+#         return y, present   # TODO: check that this does not break anything
 
 
 class Block(nn.Module):
